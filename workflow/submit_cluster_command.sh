48691cdf3e9a5ad4031875b66206fa1e94663498
jmbreda <jeremiebreda@disroot.org>
2024-05-01 07:56:23 +0200

    join TFs svd 1st comp (jed)

--- workflow/submit_cluster_command.sh
+++ workflow/submit_cluster_command.sh
@@ -10,4 +10,4 @@ snakemake -s workflow/Snakefile \
                             --ntasks {cluster.ntasks} \
                             --cpus-per-task {cluster.cpus-per-task} \
                             --output {cluster.stdout} \
-                            --error {cluster.stderr}" -n
+                            --error {cluster.stderr}"
